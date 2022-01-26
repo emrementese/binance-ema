f50321532028031043c747d5abc0037287a00b39
Emre MENTEŞE <emrementese@gmail.com>
2022-01-26 14:37:49 +0300

    fixed for pypi

--- binancema/__init__.py
+++
@@ -1,3 +0,0 @@
-""" get crypto coin informations & calculate the custom or constant indicators """
-
-__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+path = os.path.join(os.path.dirname(__file__),"requirements.txt")
+
+with open(path,"r") as req_files:
+    requirements = req_files.readlines()
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+NAME = "binance-ema"
+DESCRIPTION = ("Get crypto coin informations & calculate the custom or constant indicators. (WITH BINANCE API)")
+AUTHOR = "Emre MENTESE <emrmentese@gmail.com>"
+URL = "https://github.com/emrementese/binance-ema"
+VERSION = "0.1.0"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    license="MIT",
+    description=DESCRIPTION,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    AUTHOR=AUTHOR,
+    url=URL,
+    keywords=["Binance", "Public API","EMA","MACD","TRADE","Crypto","Coin","Indicators","BTC","ETH","USDT"],
+    install_requires=[req for req in requirements],
+    packages=find_packages(exclude=("binancema",)),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.6",
+)
