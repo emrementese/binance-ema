b439a0dc60efa9216465dad88f2a5f2d2eeb0c42
Emre MENTEŞE <emrementese@gmail.com>
2022-01-26 21:43:19 +0300

    fixed verisyon number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ NAME = "binance-ema"
 DESCRIPTION = ("Get crypto coin informations & calculate the custom or constant indicators. (WITH BINANCE API)")
 AUTHOR = "Emre MENTESE"
 URL = "https://github.com/emrementese/binance-ema"
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 setup(
     name=NAME,
